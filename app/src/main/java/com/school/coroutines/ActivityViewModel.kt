47122c544a26e517128b6e3aaf6ec0a076f5d529
Maria Bakaeva <mary396969@gmail.com>
2021-02-08 23:15:04 +0300

    version 1

--- app/src/main/java/com/school/coroutines/ActivityViewModel.kt
+++ app/src/main/java/com/school/coroutines/ActivityViewModel.kt
@@ -1,8 +1,10 @@
 package com.school.coroutines
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,24 +14,27 @@ class ActivityViewModel : ViewModel() {
     val state: LiveData<State>
         get() = _state
 
-    private val callback = object : Callback<List<MainActivity.Adapter.Item>> {
-        override fun onResponse(call: Call<List<MainActivity.Adapter.Item>>, response: Response<List<MainActivity.Adapter.Item>>) {
-            if (response.isSuccessful) {
-                response.body()?.let { _state.value = State.Loaded(it) }
-            }
-        }
-
-        override fun onFailure(call: Call<List<MainActivity.Adapter.Item>>, t: Throwable) {
-            _state.value = State.Loaded(emptyList())
-        }
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        Log.v("Error", throwable.message.toString())
     }
+
+    private val scope = CoroutineScope(SupervisorJob() + exceptionHandler)
 
     init {
         refreshData()
     }
 
     private fun refreshData() {
-        Repository.getPosts(callback)
+        scope.launch {
+            withContext(Dispatchers.IO) {
+                val result = Repository.getPosts()
+                if (result.isSuccessful) {
+                    _state.postValue(result.body()?.let { State.Loaded(it) })
+                } else {
+                    _state.postValue(State.Loaded(emptyList()))
+                }
+            }
+        }
     }
 
     fun processAction(action: Action) {
--- app/src/main/java/com/school/coroutines/Repository.kt
+++ app/src/main/java/com/school/coroutines/Repository.kt
@@ -1,37 +1,43 @@
 package com.school.coroutines
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import kotlinx.coroutines.Deferred
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 object Repository {
 
-    fun getPosts(callBack: Callback<List<MainActivity.Adapter.Item>>) = NetworkSource.getPosts(callBack)
+    suspend fun getPosts() = NetworkSource.getPosts()
 
     object NetworkSource {
         private interface IPostApi {
             @GET("/posts")
-            fun getPosts(): Call<List<MainActivity.Adapter.Item>>
+            fun getPosts(): Deferred<Response<List<MainActivity.Adapter.Item>>>
         }
 
         private val okHttpClient = OkHttpClient.Builder()
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                level = HttpLoggingInterceptor.Level.BODY
-            })
-            .build()
+                .addInterceptor(HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                })
+                .build()
 
         private val retrofit = Retrofit.Builder()
-            .baseUrl("https://jsonplaceholder.typicode.com")
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(okHttpClient)
-            .build()
+                .baseUrl("https://jsonplaceholder.typicode.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .client(okHttpClient)
+                .build()
 
         private val postApi = retrofit.create(IPostApi::class.java)
 
-        fun getPosts(callBack: Callback<List<MainActivity.Adapter.Item>>) = postApi.getPosts().enqueue(callBack)
+        suspend fun getPosts() = postApi
+                .getPosts()
+                .await()
     }
 }
